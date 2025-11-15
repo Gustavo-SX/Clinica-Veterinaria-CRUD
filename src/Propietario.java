4a4c814eec7a1c229bcdbb82beea9c7fb0ba8934
brunocapamga <brunocapamga@mail.com>
2025-11-15 11:39:30 -0300

    brunocommit3

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,2 +1,79 @@
 public class Propietario {
+
+    //atributos
+    private String nome;
+    private int cpf;
+    private int telefone;
+    private String endereco;
+    private String nomedoanimal;
+    private int iddoanimal;
+
+    //construtor
+    public Propietario(String nome, int cpf, int telefone, String endereco, String nomedoanimal, int iddoanimal) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.nomedoanimal = nomedoanimal;
+        this.iddoanimal = iddoanimal;
+    }
+
+    //gets e sets
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNomedoanimal() {
+        return nomedoanimal;
+    }
+
+    public void setNomedoanimal(String nomedoanimal) {
+        this.nomedoanimal = nomedoanimal;
+    }
+
+    public int getIddoanimal() {
+        return iddoanimal;
+    }
+
+    public void setIddoanimal(int iddoanimal) {
+        this.iddoanimal = iddoanimal;
+    }
+
+    //exibir dethales do proprietario
+    public void exibir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("Nomedoanimal: " + nomedoanimal);
+        System.out.println("Iddoanimal: " + iddoanimal);
+    }
 }
--- src/Veterinario.java
+++ src/Veterinario.java
@@ -1,2 +1,50 @@
 public class Veterinario {
+    //atributos
+    private int idveterinario;
+    private String nome;
+    private int cpf;
+    private int telefone;
+
+
+    //construtor
+    public Veterinario(int idveterinario, String nome, int cpf, int telefone) {
+        this.idveterinario = idveterinario;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+
+    }
+    //gets e sets
+    public int getidveterinario() {
+        return idveterinario;
+    }
+    public void setidveterinario(int idveterinario) {
+        this.idveterinario = idveterinario;
+    }
+    public String getnome() {
+        return nome;
+    }
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+    public int getcpf() {
+        return cpf;
+    }
+    public void setcpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public int gettelefone() {
+        return telefone;
+    }
+    public void settelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    //exibir detalhes do veterinario
+    public void exibir(){
+        System.out.printf("idveterinario: ", idveterinario);
+        System.out.printf("nome: ", nome);
+        System.out.printf("cpf: ", cpf);
+        System.out.printf("telefone: ", telefone);
+    }
+
 }
